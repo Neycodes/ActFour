52f860b5724b3c4254f1924c20b623efb25a54bc
Nico <Nicollosa35@gmail.com>
2022-09-11 09:30:49 +0800

    Ready to pass

--- src/com/nicoo/Main.java
+++ src/com/nicoo/Main.java
@@ -13,13 +13,11 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
     static Scanner read = new Scanner(System.in);
     static Buyer buyer = new Buyer();
     public static void main(String[] args) {
         System.out.println("Is this shoe available?: " );
         buyer.asking();
-
 
         Seller seller = new Seller();
 
@@ -54,10 +52,9 @@ public class Main {
     }
 
     static class Seller extends Billing{
-
         boolean isAvail;
         void isAvailable() {
-            isAvail = setShoeList().contains(buyer.isThere);
+            isAvail = setShoeList().contains(buyer.isThere.toUpperCase());
         }
         void available(){
             if (isAvail) {
@@ -67,38 +64,39 @@ public class Main {
                 System.exit(0);
             }
         }
-
     }
 
     static class ShoeShelves  {
         ArrayList<String> shoeList = new ArrayList<>();
         ArrayList<String> setShoeList() {
-            shoeList.add("Airforce 1");
-            shoeList.add("Airforce 2");
-            shoeList.add("Airforce 3");
-            shoeList.add("Airforce 4");
-            shoeList.add("Airforce 5");
-            shoeList.add("Airforce 6");
-            shoeList.add("Airforce 7");
-            shoeList.add("Airforce 8");
-            shoeList.add("Airforce 9");
+            shoeList.add("NIKE AIRFORCE");
+            shoeList.add("NIKE AIR MAX");
+            shoeList.add("NIKE REVOLUTION");
+            shoeList.add("NIKE QUEST");
+            shoeList.add("NIKE AIR JORDAN");
+            shoeList.add("VANS OLD SKOOL");
+            shoeList.add("ONITSUKA TIGER MEXICO");
+            shoeList.add("ONITSUKA TIGER ACROMOUNT");
+            shoeList.add("ADIDAS FORUM");
+            shoeList.add("ADIDAS STAN SMITH");
+            shoeList.add("ADIDAS CONTINENTAL");
+
 
             return shoeList;
         }
     }
 
     static class PriceList extends ShoeShelves {
-        int[] shoePrice = new int[]{1100, 2100, 1300, 4100, 5100, 1600, 1700, 1800, 1900, 9011};
+        int[] shoePrice = new int[]{3999, 4599, 3599, 4999, 5999, 3799, 3800, 4900, 3499,5599,4399};
          void sPrice(){
              System.out.println(    );
-             System.out.println("The price of "+ buyer.isThere +" is P"+shoePrice[(setShoeList().indexOf(buyer.isThere))]);
+             System.out.println("The price of "+ buyer.isThere +" is P"+shoePrice[(setShoeList().indexOf(buyer.isThere.toUpperCase(Locale.ROOT)))]);
          }
     }
 
         static class Billing extends PriceList{
-
         void invoice(){
-            String[] inv = new String[]{" ","NAME                           " + buyer.myName.toUpperCase(Locale.ROOT) , " ", "ITEM                           "+ buyer.isThere.toUpperCase(Locale.ROOT)," ",  " "," ", "TOTAL                           P    "+ (shoePrice[setShoeList().indexOf(buyer.isThere)]), " ", " "};
+            String[] inv = new String[]{" ", "NAME                           " + buyer.myName.toUpperCase(Locale.ROOT) , " ", "ITEM                           "+ buyer.isThere.toUpperCase(Locale.ROOT)," ",  " "," ", "TOTAL                           P    "+ (shoePrice[setShoeList().indexOf(buyer.isThere.toUpperCase(Locale.ROOT))]), " ", " "};
             System.out.println();
             System.out.println(" ============================================");
             System.out.println("                    Invoice");
