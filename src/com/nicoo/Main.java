0ddc5a367b0622805822f2c03c766be017be1fd2
Nico <Nicollosa35@gmail.com>
2022-09-11 10:35:36 +0800

    Ready to pass

--- src/com/nicoo/Main.java
+++ src/com/nicoo/Main.java
@@ -13,44 +13,46 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    // static objects, so that other classes can access them.
+    // static objects and variables, so that other classes can access them.
     static Scanner read = new Scanner(System.in);
     static Buyer buyer = new Buyer();
+    static String shoeName;
+    static String customerName;
     public static void main(String[] args) {
-        System.out.println("Is this shoe available?: " );
+        System.out.print("Is this shoe available?: " );
         buyer.asking();
         // seller object
         Seller seller = new Seller();
         // The transaction process between the Buyer and the Seller, and also the Seller and the other classes functions.
         seller.isAvailable();
         seller.available();
-        seller.sPrice();
+        seller.sPrice(shoeName);
 
         System.out.println("How may I address you?: " );
         buyer.name();
 
-        buyer.buy(buyer.isThere);
-        seller.invoice(buyer.isThere);
+        buyer.buy(shoeName);
+        seller.invoice(shoeName);
         buyer.pay();
     }
 // All of these static classes are connected to each other by inheritance.
     // Buyer CLass consists of String variables, and methods/functions
     static class Buyer{
-        String myName;
-        String isThere;
         void asking(){
-            isThere = read.nextLine();
+            shoeName = read.nextLine();
         }
 
         void buy(String s){
             System.out.println();
             System.out.println("Buying " + s.toUpperCase(Locale.ROOT));
         }
+
         void pay(){
             System.out.println("Payment sent ");
         }
+
         void name(){
-            myName = read.nextLine();
+           customerName = read.nextLine();
         }
     }
     // Seller class consists of methods/ functions for checking the shoe shelf, and return a response if the shoe is available.
@@ -58,13 +60,13 @@ public class Main {
         boolean isAvail;
         // isAvailable method is used to setting the isAvail boolean to true or false.
         void isAvailable() {
-            isAvail = setShoeList().contains(buyer.isThere.toUpperCase());
+            isAvail = setShoeList().contains(shoeName.toUpperCase());
         }
         void available(){
             if (isAvail) {
-                System.out.println( "Yes "+buyer.isThere.toUpperCase(Locale.ROOT)+" is available");
+                System.out.println( "Yes "+ shoeName.toUpperCase(Locale.ROOT)+" is available");
             }else{
-                System.out.println("Sorry, "+ buyer.isThere.toUpperCase(Locale.ROOT) + " is not available yet");
+                System.out.println("Sorry, "+ shoeName.toUpperCase(Locale.ROOT) + " is not available yet");
                 System.exit(0);
             }
         }
@@ -92,15 +94,15 @@ public class Main {
     static class PriceList extends ShoeShelf {
         int[] shoePrice = new int[]{3999, 4599, 3599, 4999, 5999, 3799, 3800, 4900, 3499,5599,4399};
         // This method prints out the price of a shoe,
-         void sPrice(){
+         void sPrice(String shoe){
              System.out.println(    );
-             System.out.println("The price of "+ buyer.isThere.toUpperCase(Locale.ROOT) +" is P"+shoePrice[(setShoeList().indexOf(buyer.isThere.toUpperCase(Locale.ROOT)))]);
+             System.out.println("The price of "+ shoe.toUpperCase(Locale.ROOT) +" is P"+shoePrice[(setShoeList().indexOf(shoe.toUpperCase(Locale.ROOT)))]);
          }
     }
     // Billing class, contains a method that prints out the invoice with the billing info.
         static class Billing extends PriceList{
         void invoice(String item){
-            String[] inv = new String[]{" ", "NAME                           " + buyer.myName.toUpperCase(Locale.ROOT) , " ", "ITEM                           "+ item.toUpperCase(Locale.ROOT)," ",  " "," ", "TOTAL                          P    "+ (shoePrice[setShoeList().indexOf(item.toUpperCase(Locale.ROOT))]), " ", " "};
+            String[] inv = new String[]{" ", "NAME                           " + customerName.toUpperCase(Locale.ROOT) , " ", "ITEM                           "+ item.toUpperCase(Locale.ROOT)," ",  " "," ", "TOTAL                          P    "+ (shoePrice[setShoeList().indexOf(item.toUpperCase(Locale.ROOT))]), " ", " "};
             System.out.println();
             System.out.println(" ============================================");
             System.out.println("                    Invoice");
@@ -111,12 +113,3 @@ public class Main {
         }
     }
 }
-
-
-
-
-
-
-
-
-
